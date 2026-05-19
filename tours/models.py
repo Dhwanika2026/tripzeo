08e47384cbfdc0686de065fbe03930b125c47167
Dhwanika <dhwanikaoverseas@gmail.com>
2026-05-19 18:42:45 +0530

    home page related changes

--- tours/models.py
+++ tours/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Package(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='packages/')
+    offer = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
